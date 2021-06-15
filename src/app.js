654e23b0b085210829218d7331027fcf91d3366e
AnushkaBhardwaj <anu23bhardwaj@gmail.com>
2021-06-15 15:14:00 +0530

    test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const registerRouter = require("./routers/register");
 const loginRouter = require('./routers/login');
 const usersRouter = require('./routers/users');
+const poerfoliosRouter = require('./routers/portfolios');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -23,47 +24,7 @@ app.use(express.static(viewsPath));
 app.get('/' , (req,res)=>{
     res.render("index");
 })
-app.post('/jobs' , async(req,res)=>{
-    try{
-        const registerJob = await new job({
-            companyName: req.body.companyName,
-            profile: req.body.profile,
-            jobDescription: req.body.jobDescription,
-            experience: req.body.experience,
-            jobDetails: req.body.jobDetails,
-            jobLocation: req.body.jobLocation,
-        })
-        registerJob.save().then(()=>{
-            console.log(registerJob);
-            res.send(registerJob)
-        })
-    }
-   
-    catch(error){
-        res.status(400).send(error);
-    }
-})
-app.patch('/jobs/update/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['companyName', 'jobDescription', 'profile', 'experience', 'jobDetails', 'jobLocation']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const Job = await job.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    
-        if (!Job) {
-            return res.status(404).send()
-        }
-
-        res.send(Job)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
 
 app.listen(port , ()=>{
     console.log('Site is up on port ' + port);
