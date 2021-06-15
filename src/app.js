929d3bb82917b2b487e22e2ab6e5ac1ce59d3b2b
fvarmint <chutanidishan@gmail.com>
2021-06-15 17:31:08 +0530

    admin-bro commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const registerRouter = require("./routers/register");
 const loginRouter = require('./routers/login');
 const usersRouter = require('./routers/users');
+const adminRouter = require('./routers/admin');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,7 +15,9 @@ app.use(express.json());
 app.use(registerRouter);
 app.use(loginRouter);
 app.use(usersRouter);
-app.use(express.urlencoded({extended:false}))
+app.use('/admin' , adminRouter)
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended:false}));
 
 app.set('view engine' , 'hbs')
 app.set('views' , viewsPath)
