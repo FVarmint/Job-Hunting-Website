b0408f2f984410d7f8ad75df0656d0f7cfc8238d
fvarmint <chutanidishan@gmail.com>
2021-07-24 17:01:20 +0530

    navbar and grid improvement

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -10,50 +10,93 @@ import '../assets/css/Sidebar.css';
 
 const Header = (props) => {
 
-  const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
+  const [nav , setnav] = useState(false);
+
+  const changeBackground = () =>{
+    if(window.scrollY >= 50){
+      setnav(true);
+    }
+    else{
+      setnav(false);
+    }
+  }
+
+  window.addEventListener('scroll' , changeBackground);
 
     const showSidebar = () => setSidebar(!sidebar)
 
     return (
-      
-      <Navbar bg="light" expand="lg">
-        <Link to='#' className='menu-bars'>
-                <FaBars onClick={showSidebar}/>
-            </Link>
-      <Navbar.Brand href="#">{props.title}</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-      </Navbar.Collapse>
+    //   <Navbar bg="light" expand="lg">
+    //     <Link to='#' className='menu-bars'>
+    //             <FaBars onClick={showSidebar}/>
+    //         </Link>
+    //   <Navbar.Brand href="#">{props.title}</Navbar.Brand>
+    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //   <Navbar.Collapse id="basic-navbar-nav">
+    //   </Navbar.Collapse>
+    //   <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+    //         <ul className='nav-menu-items' onClick={showSidebar}>
+                
+    //             <li className="navbar-toggle">
+    //                 <Link to="#" className='menu-bars'>
+    //                     <AiOutlineClose />
+    //                 </Link>
+    //             </li>
+    //             <Link to ="#" className='menu-bars'>
+    //                   <AiOutlineUser />
+    //                 <Navbar.Brand href="#">{props.loggedInUser}</Navbar.Brand>
+    //                 </Link>
+    //             {SidebarData.map((item, index) =>{
+    //               return (
+    //                 <li key={index} className={item.cName}>
+    //                   <Link to={item.path}>
+    //                   {item.icon}
+    //                   <span>{item.title}</span>
+    //                   </Link>
+    //                   </li>
+    //               )
+    //             } )}
+    //         </ul>
+    //     </nav> 
+    //     <Nav className="mr-auto">
+    //       <Nav.Link href="#home">Home</Nav.Link>
+    //       <Nav.Link href="#link">Link</Nav.Link>
+    //       <Nav.Link href="/logout">Logout</Nav.Link>
+    //     </Nav>
+    // </Navbar>
+    <nav className = {nav ? 'nav active' : 'nav'}>
+      <Link to='#' className='menu-bars'>
+          <FaBars onClick={showSidebar}/>         
+      </Link>
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
+      <a href='#' className='logo'>
+        <img src='' alt=''/>
+      </a>
+      <ul className='nav-menu-items' onClick={showSidebar}>
                 
                 <li className="navbar-toggle">
-                    <Link to="#" className='menu-bars'>
-                        <AiOutlineClose />
-                    </Link>
-                </li>
-                <Link to ="#" className='menu-bars'>
-                      <AiOutlineUser />
-                    <Navbar.Brand href="#">{props.loggedInUser}</Navbar.Brand>
-                    </Link>
-                {SidebarData.map((item, index) =>{
+                     <Link to="#" className='menu-bars'>
+                         <AiOutlineClose />
+                     </Link>
+                 </li>
+                 <Link to ="#" className='menu-bars'>
+                       <AiOutlineUser />
+                     <Navbar.Brand href="#">{props.loggedInUser}</Navbar.Brand>
+                     </Link>
+                 {SidebarData.map((item, index) =>{
                   return (
-                    <li key={index} className={item.cName}>
-                      <Link to={item.path}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                      </Link>
-                      </li>
-                  )
-                } )}
-            </ul>
-        </nav> 
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <Nav.Link href="/logout">Logout</Nav.Link>
-        </Nav>
-    </Navbar>
+                     <li key={index} className={item.cName}>
+                       <Link to={item.path}>
+                       {item.icon}
+                       <span>{item.title}</span>
+                       </Link>
+                       </li>
+                   )
+                 } )}
+             </ul></nav>
+             <Link className="nav-logout" to="/logout">Logout</Link>
+    </nav>
     )
 }
 
--- server/client/src/components/MainPage.js
+++ server/client/src/components/MainPage.js
@@ -2,59 +2,23 @@ import React , { useState , useEffect} from 'react'
 import axios from 'axios';
 import Cookies from 'js-cookie'
 import Header from './Header';
-import Sidebar from './Sidebar'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import { purple } from '@material-ui/core/colors';
+import { purple, red } from '@material-ui/core/colors';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Navbar, Nav, NavDropdown, Form, FormControl} from 'react-bootstrap';
-import Footer from './Footer';
-import PageHeader from "./PageHeader"
-import IndexNavbar from './IndexNavbar';
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }),
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  }
+}))
 
 const MainPage = () => {
 
@@ -103,13 +67,6 @@ const MainPage = () => {
     fetchDataPortfolios();
   },[])
 
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   const theme = createTheme({
     palette: {
       primary: {
@@ -123,14 +80,16 @@ const MainPage = () => {
     },
   });
 
+  const classes = useStyles();
+
     return (
         <>
-        {/* <Sidebar title="Job Hunting Website" searchBar={false} loggedInUser={user.username}/> */}
+        <div id='main'>
         <Header loggedInUser={user.username}/>
 <Container spacing={10}>
 <Grid container spacing={10}>
   <Grid item xs={12} sm={6} md={4}>
-    <Paper>
+    <Paper className={classes.paper}>
       <div className="container">
     <div className="users">
     <ThemeProvider theme={theme}>
@@ -159,10 +118,10 @@ const MainPage = () => {
     </Paper>
   </Grid>
   <Grid item xs={12} sm={6} md={4}>
-  <Paper>
+  <Paper className={classes.paper}>
   <div className="container">
   {jobs.length===0 ? <h3>No Jobs Found</h3>
-:<h3>Jobs Uploaded By User</h3>}
+:<h3>Jobs Uploaded By You</h3>}
 {jobs.length===0 ? <div className="mt-4">
   <a href="/jobs">  
     <button type="button" className="btn btn-primary">Add Job</button>  
@@ -173,18 +132,18 @@ const MainPage = () => {
     <h5 className="card-title">{job.profile}</h5>
     <h6 className="card-subtitle mb-2 text-muted">{job.companyName}</h6>
     <p className="card-text">{job.description}</p>
-    <a href={`/updatejobs/${job._id}`} className="card-link">update job</a>
-    <a href={`/deletejob/${job._id}`} className="card-link">delete job</a>
+    <a href={`/updatejobs/${job._id}`} className="card-link">Update</a>
+    <a href={`/deletejob/${job._id}`} className="card-link">Delete</a>
     <a href={`/displayapplicationsbyjob/${job._id}`} className="card-link">Applications</a>
   </div>
 </div>
 ))}</div></Paper>
   </Grid>
   <Grid item xs={12} sm={6} md={4}>
-  <Paper>
+  <Paper className={classes.paper}>
   <div className="container">
   {portfolios.length===0 ? <h3>No Portfolio Found</h3>
-:<h3>Portfolios Created By User</h3>}
+:<h3>Your Portfolios</h3>}
 {portfolios.length===0 ? <div className="mt-4">
   <a href="/portfolio">  
     <button type="button" className="btn btn-primary">Create Portfolio</button>  
@@ -195,8 +154,8 @@ const MainPage = () => {
           <h5 className="card-title">{portfolio.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{portfolio.profile}</h6>
           <p className="card-text">{portfolio.education}</p>
-          <a href={`/updateportfolios/${portfolio._id}`} className="card-link">update portfolio</a>
-          <a href={`/deleteportfolio/${portfolio._id}`} className="card-link">delete portfolio</a>
+          <a href={`/updateportfolios/${portfolio._id}`} className="card-link">Update</a>
+          <a href={`/deleteportfolio/${portfolio._id}`} className="card-link">Delete</a>
         </div>
       </div>
       ))}
@@ -205,6 +164,7 @@ const MainPage = () => {
   </Grid>
   </Grid>
   </Container>
+  </div>
         </>
     )
 }
